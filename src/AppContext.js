9feeee7e22c653b23bb9e8a6ecd00f6ae6e44546
Lily <lilywuyanru@gmail.com>
2021-05-10 22:47:02 -0400

    fix crash on refresh

--- src/AppContext.js
+++ src/AppContext.js
@@ -3,50 +3,96 @@ import React, { useState, createContext, useCallback, useEffect} from 'react';
 const AppContext = createContext([{}, () => {}]);
 
 const AppContextProvider = ({children}) => {
-  const [room, setRoom] = useState(null);
-  const [connecting, setConnecting] = useState(false);
-  const [username, setUsername] = useState("");
-  const [workout, setWorkout] = useState({"workoutName": "", "exercises": [{"time": 1, "exercise":""}]})
-  const [roomName, setRoomName] = useState("");
-  const [roomState, setRoomState] = useState(null);
+  // useEffect(() => {
+    localStorage.setItem("Room", null);
+    localStorage.setItem("Connecting", JSON.stringify(false));
+    localStorage.setItem("Username", JSON.stringify(""));
+    localStorage.setItem("Workout", JSON.stringify({"workoutName": "", "exercises": [{"time": 1, "exercise":""}]}));
+    localStorage.setItem("RoomState", JSON.stringify(null));
+    localStorage.setItem("RoomTitle", JSON.stringify(""));
+    localStorage.setItem("UserId", JSON.stringify(null));
+  // })
+
+  const [room, setRoom] = useState(JSON.parse(localStorage.getItem('Room')));
+  const [connecting, setConnecting] = useState(JSON.parse(localStorage.getItem('Connecting')));
+  const [username, setUsername] = useState(JSON.parse(localStorage.getItem('Username')));
+  const [workout, setWorkout] = useState(JSON.parse(localStorage.getItem('Workout')))
+  const [roomName, setRoomName] = useState(JSON.parse(localStorage.getItem('RoomName')));
+  const [roomState, setRoomState] = useState(JSON.parse(localStorage.getItem('RoomName')));
   const [roomTitle, setRoomTitle] = useState("")
   const [openSideBar, setOpenSideBar] = useState(true)
   const [userId, setUserId] = useState('')
   const [openAuthDialog, setOpenAuthDialog] = useState(false);
   const [isSignUp, setIsSignUp] = useState(false);
 
+  // useEffect(() => {
+  //   localStorage.setItem("RoomName", JSON.stringify(roomName));
+  // }, [roomName]);
+
+  // useEffect(() => {
+  //   localStorage.setItem("RoomState", JSON.stringify(roomState));
+  // }, [roomState]);
+
+  // useEffect(() => {
+  //   localStorage.setItem("roomTitle", JSON.stringify(roomTitle));
+  // }, [roomTitle]);
+
   const handleSetRoom = (room) => {
     setRoom(room)
+    localStorage.setItem("Room", JSON.stringify(
+      // {
+      // name: room.name,
+      // sid: room.sid,
+      // workoutID: room.workoutId,
+      // workoutType: room.workoutType,
+      // localParticipant: {
+      //   audioTracks: room.localParticipant.audioTracks,
+      //   dataTracks: room.localParticipant.dataTracks,
+      //   tracks: room.localParticipant.tracks,
+      //   videoTracks: room.localParticipant.videoTracks,
+      //   signalingRegion: room.localParticipant.signalingRegion,
+      // }
+    // }
+    room
+    ));
   }
   const handleSetConnecting = (connecting) => {
     setConnecting(connecting)
+    localStorage.setItem("Connecting", JSON.stringify(connecting));
   }
   const handleSetUsername = (username) => {
     setUsername(username)
+    localStorage.setItem("Username", JSON.stringify(username));
   }
   const handleSetRoomName = (roomname) => {
     setRoomName(roomname)
+    localStorage.setItem("RoomName", JSON.stringify(roomname));
   }
   const handleSetRoomState = (roomState) => {
     setRoomState(roomState)
+    localStorage.setItem("RoomState", JSON.stringify(roomState));
   }
   const handleSetRoomTitle = (roomTitle) => {
     setRoomTitle(roomTitle)
+    localStorage.setItem("RoomTitle", JSON.stringify(roomTitle));
   }
   const handleSetWorkout = (workout) => {
     setWorkout(workout)
+    localStorage.setItem("Workout", JSON.stringify(workout));
   }
   const handleOpenSideBar = () => {
     setOpenSideBar(!openSideBar)
   }
   const handleSetUserId = (userId) => {
     setUserId(userId)
+    localStorage.setItem("UserId", JSON.stringify(userId));
   }
   const handleSetOpenAuthDialog = (val) => {
     setOpenAuthDialog(val);
   }
   const handleSetIsSignUp = (val) => {
     setIsSignUp(val);
+    localStorage.setItem("IsSignUp", JSON.stringify(val));
   }
 
   // const createRoom = (room_code) => {
@@ -57,21 +103,27 @@ const AppContextProvider = ({children}) => {
 
   const disconnectRoom = () => {
     setRoomName("")
+    localStorage.setItem("RoomName", "");
     setUsername("")
+    localStorage.setItem("Username", "");
     setRoomState(null)
+    localStorage.setItem("RoomState", "");
   }
 
   const joinRoom = () => {
     // check if room exists here TODO
     setRoomState('join')
+    localStorage.setItem("RoomState", "join");
   }
 
   const handleUsernameChange = useCallback((event) => {
     handleSetUsername(event.target.value);
+    localStorage.setItem("Username", JSON.stringify(event.target.value));
   }, []);
 
   const handleRoomTitle = useCallback((event) => {
     setRoomTitle(event.target.value);
+    localStorage.setItem("RoomTitle", JSON.stringify(event.target.value));
   }, []);
 
   const makeCustomRoom = async (event) => {
@@ -83,8 +135,11 @@ const AppContextProvider = ({children}) => {
     });
     const roomCode = await res.text();
     setRoomName(roomCode)
+    localStorage.setItem("RoomName", JSON.stringify(roomCode));
     setUsername("Leader")
+    localStorage.setItem("Username", "Leader");
     setRoomState('make_custom')
+    localStorage.setItem("RoomStae", "make_custom");
   }
 
   const handleRoomNameChange = useCallback((event) => {
