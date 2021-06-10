3e263cb32b9d60b6a3a4ad24108bb51f4eee5e5c
Matthew Wong <matthewwong@Matthews-MacBook-Air.local>
2021-06-10 15:28:05 -0400

    Refactor BottomControl from Room.js

--- src/Room/Room.js
+++ src/Room/Room.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useContext, useState, useRef } from "react";
 import Participant from "./Participant/Participant";
 import SideBar from "./SideBar/SideBar";
+import BottomControl from "./BottomControl/BottomControl"
 import { AppContext } from "../AppContext";
-import {Grid, Typography, Box, IconButton, BottomNavigation, BottomNavigationAction, withStyles} from '@material-ui/core';
-import { ArrowForward, ArrowBack, Videocam, VideocamOff, Mic, MicOff, ChevronLeft, ChevronRight, YouTube, FitnessCenter} from '@material-ui/icons';
+import {Grid, Typography, Box} from '@material-ui/core';
 import Video from './Video/Video';
 import { sckt } from '../Socket';
 import { makeStyles } from "@material-ui/core/styles";
@@ -13,11 +13,9 @@ import { Redirect} from "react-router-dom";
 const Room = (props) => {
   const [participants, setParticipants] = useState([]);
   const [participantPage, setParticipantPage] = useState(0);
-  const ppp = 4; // participants per page
   const [leaderParticipantIDs, setLeaderParticipantIDs] = useState([]);
-  const { username, room, handleLeaveRoom, userId, openSideBar, handleOpenSideBar, roomProps, updateRoomProps, workoutType, setWorkoutType, videoProps, updateVideoProps, sendRoomState } = useContext(AppContext);
-  const [vid, setVid] = useState((room) ? room.localParticipant.videoTracks.values().next().value.isTrackEnabled : false);
-  const [mic, setMic] = useState((room) ? room.localParticipant.audioTracks.values().next().value.isTrackEnabled : false);
+  const ppp = 4; // participants per page
+  const { username, room, handleLeaveRoom, userId, openSideBar, roomProps, updateRoomProps, workoutType, videoProps, updateVideoProps } = useContext(AppContext);
 
   // Initializing Room Stuff
   useEffect(() => {
@@ -207,32 +205,6 @@ const Room = (props) => {
     }
   };
 
-  const handleMic = () => {
-    if (!room) return;
-    room.localParticipant.audioTracks.forEach(track => {
-      (mic) ? track.track.disable() : track.track.enable()
-    });
-    setMic(!mic);
-  };
-
-  const handleVid = () => {
-    if (!room) return;
-    room.localParticipant.videoTracks.forEach(track => {
-      (vid) ? track.track.disable() : track.track.enable()
-    });
-    setVid(!vid);
-  };
-
-  const handleParticipantPage = (pageDelta) => {
-    if (!room) return;
-    let all_participants = [...participants, room.localParticipant];
-    all_participants = (workoutType === 'yt') ? all_participants : all_participants.filter((participant) => participant.sid !== leaderParticipantIDs[0])
-    const newPageNum = participantPage + pageDelta;
-    if (all_participants.slice(newPageNum * ppp, newPageNum * ppp + ppp).length > 0) {
-      setParticipantPage(newPageNum)
-    }
-  }
-
   const useStyles = makeStyles(theme => ({
     content: {
       flexGrow: 1,
@@ -251,21 +223,6 @@ const Room = (props) => {
     },
   }));
   const classes = useStyles();
-
-  const handleChangeWorkoutType = (value) => {
-    const newWorkoutType = value ? 'yt' : 'vid';
-    sendRoomState({
-      eventName: 'syncWorkoutType',
-      eventParams: { workoutType: newWorkoutType }
-    }, () => {setWorkoutType(newWorkoutType)});
-  }
-
-  const CustomBottomNavigationAction = withStyles({
-    root: {
-      backgroundColor: 'rgba(0, 0, 0, 0.87)',
-      color: 'white'
-    },
-  })(BottomNavigationAction);
 
   // check if room exists
   // TODO: Add loading screen. https://stackoverflow.com/questions/56861580/how-can-i-redirect-before-render
@@ -287,56 +244,13 @@ const Room = (props) => {
             {remoteParticipants()}
           </Grid>
           <Grid item container xs={12} style={{height: "10%", width:"100%"}} alignItems="center">
-            <Grid item xs={4}>
-              <Box display="flex" justifyContent="flex-start" alignItems="center">
-                <IconButton color="secondary" size="medium" onClick={handleVid}>
-                  {vid ? <Videocam/> : <VideocamOff/>}
-                </IconButton>
-                <IconButton color="secondary" size="medium" onClick={handleMic}>
-                  {mic ? <Mic/> : <MicOff/>}
-                </IconButton>
-                {/* <IconButton>
-                  <CallEnd></CallEnd>
-                </IconButton> */}
-              </Box>
-            </Grid>
-            <Grid item xs={4}>
-              <Box display="flex" justifyContent="center" alignItems="center" l={3} r={3}>
-                <IconButton color="secondary" size="medium" onClick={() => handleParticipantPage(-1)}>
-                  <ArrowBack style={{fill: "white"}}/>
-                </IconButton>
-                <Typography color="secondary"> {participants.length + leaderParticipantIDs.length}/{participants.length + leaderParticipantIDs.length} participants {participantPage} </Typography>
-                <IconButton color="secondary" size="medium"  onClick={() => handleParticipantPage(1)}>
-                  <ArrowForward style={{fill: "white"}}/>
-                </IconButton>
-              </Box>
-            </Grid>
-            <Grid item xs={4}>
-              <Box display="flex" justifyContent="flex-end" alignItems="center">
-                {/* <IconButton color="secondary" size="medium">
-                  <Apps/>
-                </IconButton>
-                <IconButton color="secondary" size="medium">
-                  <Fullscreen/>
-                </IconButton> */}
-
-                <BottomNavigation
-                  value={workoutType === 'yt' ? 1 : 0}
-                  onChange={(event, newValue) => {
-                    handleChangeWorkoutType(newValue);
-                  }}
-                  showLabels
-                  className={classes.root}
-                  color="secondary" 
-                >
-                  <CustomBottomNavigationAction label="Custom" icon={<FitnessCenter />} />
-                  <CustomBottomNavigationAction color="secondary" label="Youtube" icon={<YouTube/>} />
-                </BottomNavigation>
-                <IconButton color="secondary" size="medium" onClick={handleOpenSideBar}>
-                  {openSideBar ? <ChevronRight /> : <ChevronLeft />}
-                </IconButton>
-              </Box>
-            </Grid>
+            <BottomControl
+              participants={participants}
+              participantPage={participantPage}
+              setParticipantPage={setParticipantPage}
+              leaderParticipantIDs={leaderParticipantIDs}
+              ppp={ppp}
+            />
           </Grid>
         </Grid>
       </Box>
