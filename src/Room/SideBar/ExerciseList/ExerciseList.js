4a6a4ae3458ba58435c9adf927e6637e0b568606
ehhfung <ehhfung@gmail.com>
2021-07-02 23:28:50 -0400

    timer countdown now shows deci-seconds to indicate counting down

--- src/Room/SideBar/ExerciseList/ExerciseList.js
+++ src/Room/SideBar/ExerciseList/ExerciseList.js
@@ -1,58 +1,86 @@
 import React from "react";
-import {useAppContext} from "../../../AppContext"
-import { Typography, LinearProgress, IconButton, Box } from '@material-ui/core';
-import { PlayArrow, Pause } from '@material-ui/icons';
+import { useAppContext } from "../../../AppContext";
+import { Typography, LinearProgress, IconButton, Box } from "@material-ui/core";
+import { PlayArrow, Pause } from "@material-ui/icons";
 
+const ExerciseList = ({ workoutTime, nextUpExercise }) => {
+  const {
+    workout,
+    sendRoomState,
+    playWorkoutState,
+    setPlayWorkoutState,
+    workoutNumber,
+    workoutCounter,
+  } = useAppContext();
 
-const ExerciseList = ({workoutTime, nextUpExercise}) => {
-    const {workout, sendRoomState, playWorkoutState, setPlayWorkoutState, workoutNumber, workoutCounter} = useAppContext();
-
-    const handleStartWorkout = () => {
-        var startWorkoutState = !playWorkoutState;
-        sendRoomState({
-            eventName: 'syncWorkoutState',
-            eventParams: { playWorkoutState: startWorkoutState }
-        }, () => setPlayWorkoutState(startWorkoutState));
-    }
-
-    const TimerProgressBarMarkup = (
-        <React.Fragment>
-            <Box display="flex" justifyContent="flex-end" paddingTop={4} >
-                <Typography variant="body1">{workoutCounter}s</Typography>
-            </Box>
-            <div><LinearProgress variant="determinate" value={workoutCounter/workoutTime * 100} /></div>
-            <Box display="flex" justifyContent="flex-end">
-                <IconButton
-                    onClick={handleStartWorkout}
-                    data-test="startWorkoutButton">
-                    {(playWorkoutState) ? <Pause/> : <PlayArrow/>}
-                </IconButton>
-            </Box>
-        </React.Fragment>
-    )
-    const exerciseListMarkup = (
-        <React.Fragment>
-            <Typography variant="body1">Now</Typography>
-            <Typography variant="h5">{workout.exercises[workoutNumber].exercise}</Typography>
-            <Typography variant="body1">Next Up</Typography>
-            {
-                nextUpExercise && nextUpExercise.length > 1 && typeof nextUpExercise != 'string' ? (
-                    nextUpExercise.filter((exercise, index) => {return index > workoutNumber}).map((exercise, index) => {
-                        return (<Typography key={index} variant="body2" data-test="nextUpExerciseComponent">{exercise}</Typography>)
-                    })
-                ) : (
-                    <Typography variant="body2" data-test="nextUpExerciseComponent">{nextUpExercise}</Typography>
-                )
-            }
-        </React.Fragment>
-    )
-
-    return (
-        <React.Fragment>
-            {TimerProgressBarMarkup}
-            {exerciseListMarkup}
-        </React.Fragment>
+  const handleStartWorkout = () => {
+    var startWorkoutState = !playWorkoutState;
+    sendRoomState(
+      {
+        eventName: "syncWorkoutState",
+        eventParams: { playWorkoutState: startWorkoutState },
+      },
+      () => setPlayWorkoutState(startWorkoutState)
     );
+  };
+
+  const TimerProgressBarMarkup = (
+    <React.Fragment>
+      <Box display='flex' justifyContent='flex-end' paddingTop={4}>
+        <Typography variant='body1'>{workoutCounter}s</Typography>
+      </Box>
+      <div>
+        <LinearProgress
+          variant='determinate'
+          value={(workoutCounter / workoutTime) * 100}
+        />
+      </div>
+      <Box display='flex' justifyContent='flex-end'>
+        <IconButton onClick={handleStartWorkout} data-test='startWorkoutButton'>
+          {playWorkoutState ? <Pause /> : <PlayArrow />}
+        </IconButton>
+      </Box>
+    </React.Fragment>
+  );
+  const exerciseListMarkup = (
+    <React.Fragment>
+      <Typography variant='body1'>Now</Typography>
+      <Typography variant='h5'>
+        {workout.exercises[workoutNumber].exercise}
+      </Typography>
+      <Typography variant='body1'>Next Up</Typography>
+      {nextUpExercise &&
+      nextUpExercise.length > 1 &&
+      typeof nextUpExercise != "string" ? (
+        nextUpExercise
+          .filter((exercise, index) => {
+            return index > workoutNumber;
+          })
+          .map((exercise, index) => {
+            return (
+              <Typography
+                key={index}
+                variant='body2'
+                data-test='nextUpExerciseComponent'
+              >
+                {exercise}
+              </Typography>
+            );
+          })
+      ) : (
+        <Typography variant='body2' data-test='nextUpExerciseComponent'>
+          {nextUpExercise}
+        </Typography>
+      )}
+    </React.Fragment>
+  );
+
+  return (
+    <React.Fragment>
+      {TimerProgressBarMarkup}
+      {exerciseListMarkup}
+    </React.Fragment>
+  );
 };
 
 export default ExerciseList;
--- src/Room/SideBar/SideBar.js
+++ src/Room/SideBar/SideBar.js
@@ -1,43 +1,51 @@
 import React, { useState, useEffect } from "react";
-import ExerciseList from "./ExerciseList/ExerciseList.js"
-import { sckt } from '../.././Socket';
+import ExerciseList from "./ExerciseList/ExerciseList.js";
+import { sckt } from "../.././Socket";
 import { useAppContext } from "../../AppContext";
-import Chat from './Chat/Chat';
-import { Drawer, Typography, IconButton, Box, Grid, Tab, Tabs } from '@material-ui/core';
-import { Link } from '@material-ui/icons';
+import Chat from "./Chat/Chat";
+import {
+  Drawer,
+  Typography,
+  IconButton,
+  Box,
+  Grid,
+  Tab,
+  Tabs,
+} from "@material-ui/core";
+import { Link } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props;
 
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box style={{ height: "100%" }}>
-                    <div style={{ height: "100%" }}>{children}</div>
-                </Box>
-            )}
-        </div>
-    );
+  return (
+    <div
+      role='tabpanel'
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box style={{ height: "100%" }}>
+          <div style={{ height: "100%" }}>{children}</div>
+        </Box>
+      )}
+    </div>
+  );
 }
 
 TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
 };
 
 function a11yProps(index) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
 }
 
 // const useStyles = makeStyles((theme) => ({
@@ -47,130 +55,179 @@ function a11yProps(index) {
 //     },
 // }));
 
-const SideBar = ({
-    currUser,
-    users,
-    isYoutube,
-    drawerWidth
-}) => {
-    const { workout, openSideBar, playWorkoutState, workoutNumber, setWorkoutNumber, workoutCounter, setWorkoutCounter, roomName } = useAppContext();
-    const [workoutTime, setWorkoutTime] = useState(workout.exercises[workoutNumber].time);
-    const [nextUpExercise, setNextUpExercise] = useState(workout.exercises.map((workout, index) => { return workout.exercise }));
-    const [messages, setMessages] = useState([]);
-    const [sideBarType, setSideBarType] = useState(0);
+const SideBar = ({ currUser, users, isYoutube, drawerWidth }) => {
+  const {
+    workout,
+    openSideBar,
+    playWorkoutState,
+    workoutNumber,
+    setWorkoutNumber,
+    workoutCounter,
+    setWorkoutCounter,
+    roomName,
+  } = useAppContext();
+  const [workoutTime, setWorkoutTime] = useState(
+    workout.exercises[workoutNumber].time
+  );
+  const [nextUpExercise, setNextUpExercise] = useState(
+    workout.exercises.map((workout, index) => {
+      return workout.exercise;
+    })
+  );
+  const [messages, setMessages] = useState([]);
+  const [sideBarType, setSideBarType] = useState(0);
 
-    //Also does scrolliung
-    useEffect(() => {
-        const handler = (message) => {
-            setMessages(messages => [...messages, message]);
-            let cht = document.getElementById("chat");
-            if (cht) cht.scrollTop = cht.scrollHeight;
-        }
-        sckt.socket.on('message', handler);
-        return () => sckt.socket.off('message', handler);
-    }, []);
+  //Also does scrolliung
+  useEffect(() => {
+    const handler = (message) => {
+      setMessages((messages) => [...messages, message]);
+      let cht = document.getElementById("chat");
+      if (cht) cht.scrollTop = cht.scrollHeight;
+    };
+    sckt.socket.on("message", handler);
+    return () => sckt.socket.off("message", handler);
+  }, []);
 
-    // initialize workout
-    useEffect(() => {
-        setWorkoutTime(workout.exercises[workoutNumber].time);
-        if (workoutCounter === -1) setWorkoutCounter(workout.exercises[0].time);
-        setNextUpExercise(workout.exercises.map((workout, index) => { return workout.exercise }));
-    }, [workout, workoutCounter, setWorkoutCounter, workoutNumber]);
-
-    // countdown workout counter
-    useEffect(() => {
-        if (!playWorkoutState) return;
-        const timer = workoutCounter > 0 && setTimeout(() => setWorkoutCounter(workoutCounter - 1), 1000);
-        if (!(workoutCounter <= 0 && workoutNumber < workout.exercises.length - 1)) return;
-        setWorkoutNumber(workoutNumber + 1)
-        setWorkoutTime(workout.exercises[workoutNumber].time);
-        setWorkoutCounter(workout.exercises[workoutNumber].time);
-        return () => clearTimeout(timer)
-    }, [workoutCounter, playWorkoutState, workoutNumber, workoutTime, setWorkoutCounter, setWorkoutNumber, workout.exercises]);
-
-    const handleChange = (value) => {
-        setSideBarType(value);
-    }
-
-    const useStyles = makeStyles(theme => ({
-        drawerPaper: {
-            width: drawerWidth,
-        },
-        drawer: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
-        fullWidth: {
-            width: "100%"
-        },
-        fullHeight: {
-            height: "100%"
-        },
-        containedButton: {
-            backgroundColor: theme.palette.primary.main,
-            color: theme.palette.primary.contrastText,
-            "&:hover, &.Mui-focusVisible": { backgroundColor: theme.palette.primary.dark }
-        },
-        blackContainedButton: {
-            backgroundColor: "black",
-            color: "white",
-            "&:hover, &.Mui-focusVisible": { backgroundColor: theme.palette.primary.dark }
-        },
-        blackButton: {
-            color: "black",
-        },
-    }));
-    const classes = useStyles();
-
-    // const sideBarContentMarkup = sideBarType ?
-    //    :
-
-    const roomCode = roomName.substring(0, 6).toUpperCase();
-
-    const copyRoomCodeButtonMarkup = (
-        <IconButton color="primary" onClick={() => navigator.clipboard.writeText(roomCode)}>
-            <Link />
-        </IconButton>
-    )
-
-    return (
-        <Drawer
-            variant="persistent"
-            anchor="right"
-            open={openSideBar}
-            className={classes.drawer}
-            classes={{
-                paper: classes.drawerPaper,
-            }}
-            data-test="sidebarComponent">
-            <Box mx={2} my={2} height="95%">
-                <Typography variant="body1">{copyRoomCodeButtonMarkup}Room: {roomName.substring(0, 6).toUpperCase()}</Typography>
-                <Tabs
-                    indicatorColor="primary"
-                    textColor="primary"
-                    value={sideBarType}
-                    onChange={(event, value) => { handleChange(value) }}
-                    data-test="tabsComponent"
-                >
-                    <Tab value={0} label="Workout"  {...a11yProps(0)} />
-                    <Tab value={1} label="Chat"  {...a11yProps(1)} />
-                </Tabs>
-                <TabPanel value={sideBarType} index={0}>
-                    <Grid item>
-                        {!isYoutube && <ExerciseList workoutTime={workoutTime} nextUpExercise={nextUpExercise} data-test="exerciseListComponent"/>}
-                    </Grid>
-                </TabPanel>
-                <TabPanel value={sideBarType} index={1} style={{ height: "85%" }} id="TabPanelChat">
-                    <Chat
-                        messages={messages}
-                        currUser={currUser}
-                        users={users}
-                        data-test="chatComponent"
-                    />
-                </TabPanel>
-            </Box>
-        </Drawer>
+  // initialize workout
+  useEffect(() => {
+    setWorkoutTime(workout.exercises[workoutNumber].time);
+    if (workoutCounter === -1) setWorkoutCounter(workout.exercises[0].time);
+    setNextUpExercise(
+      workout.exercises.map((workout, index) => {
+        return workout.exercise;
+      })
     );
+  }, [workout, workoutCounter, setWorkoutCounter, workoutNumber]);
+
+  // countdown workout counter
+  useEffect(() => {
+    if (!playWorkoutState) return;
+    const timer =
+      workoutCounter > 0 &&
+      setTimeout(() => setWorkoutCounter((Math.floor((workoutCounter - 0.1)*10)/10).toFixed(1)), 100);
+    if (!(workoutCounter <= 0 && workoutNumber < workout.exercises.length - 1))
+      return;
+    setWorkoutNumber(workoutNumber + 1);
+    setWorkoutTime(workout.exercises[workoutNumber].time);
+    setWorkoutCounter(workout.exercises[workoutNumber].time);
+    return () => clearTimeout(timer);
+  }, [
+    workoutCounter,
+    playWorkoutState,
+    workoutNumber,
+    workoutTime,
+    setWorkoutCounter,
+    setWorkoutNumber,
+    workout.exercises,
+  ]);
+
+  const handleChange = (value) => {
+    setSideBarType(value);
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    fullWidth: {
+      width: "100%",
+    },
+    fullHeight: {
+      height: "100%",
+    },
+    containedButton: {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText,
+      "&:hover, &.Mui-focusVisible": {
+        backgroundColor: theme.palette.primary.dark,
+      },
+    },
+    blackContainedButton: {
+      backgroundColor: "black",
+      color: "white",
+      "&:hover, &.Mui-focusVisible": {
+        backgroundColor: theme.palette.primary.dark,
+      },
+    },
+    blackButton: {
+      color: "black",
+    },
+  }));
+  const classes = useStyles();
+
+  // const sideBarContentMarkup = sideBarType ?
+  //    :
+
+  const roomCode = roomName.substring(0, 6).toUpperCase();
+
+  const copyRoomCodeButtonMarkup = (
+    <IconButton
+      color='primary'
+      onClick={() => navigator.clipboard.writeText(roomCode)}
+    >
+      <Link />
+    </IconButton>
+  );
+
+  return (
+    <Drawer
+      variant='persistent'
+      anchor='right'
+      open={openSideBar}
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      data-test='sidebarComponent'
+    >
+      <Box mx={2} my={2} height='95%'>
+        <Typography variant='body1'>
+          {copyRoomCodeButtonMarkup}Room:{" "}
+          {roomName.substring(0, 6).toUpperCase()}
+        </Typography>
+        <Tabs
+          indicatorColor='primary'
+          textColor='primary'
+          value={sideBarType}
+          onChange={(event, value) => {
+            handleChange(value);
+          }}
+          data-test='tabsComponent'
+        >
+          <Tab value={0} label='Workout' {...a11yProps(0)} />
+          <Tab value={1} label='Chat' {...a11yProps(1)} />
+        </Tabs>
+        <TabPanel value={sideBarType} index={0}>
+          <Grid item>
+            {!isYoutube && (
+              <ExerciseList
+                workoutTime={workoutTime}
+                nextUpExercise={nextUpExercise}
+                data-test='exerciseListComponent'
+              />
+            )}
+          </Grid>
+        </TabPanel>
+        <TabPanel
+          value={sideBarType}
+          index={1}
+          style={{ height: "85%" }}
+          id='TabPanelChat'
+        >
+          <Chat
+            messages={messages}
+            currUser={currUser}
+            users={users}
+            data-test='chatComponent'
+          />
+        </TabPanel>
+      </Box>
+    </Drawer>
+  );
 };
 
 export default SideBar;
