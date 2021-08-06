8ebed87dc484f14f8653d7cf69c28d922fc59bfb
Ethan Liong <40578528+Freddington@users.noreply.github.com>
2021-08-06 12:34:17 -0400

    Update AppContext.js

--- src/AppContext.js
+++ src/AppContext.js
@@ -105,7 +105,6 @@ const AppContextProvider = ({ children }) => {
 
   const checkLoggedIn = useCallback(async () => {
     const res = await requests.checkLoggedIn();
-    console.log(res.body)
     if (!res.ok) {
       setUserId('');
       setIsLoggedIn(false);
