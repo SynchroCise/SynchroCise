eab582f56090f478d41715854f5279babd1063de
Matthew Wong <matthewwong@Matthews-MacBook-Air.local>
2021-08-06 13:56:08 -0400

    Reconfigure buildoptions

--- src/utils/jitsi.js
+++ src/utils/jitsi.js
@@ -28,12 +28,23 @@ export const useJitsi = () => {
 export const buildOptions = (roomName, tenant) => {
   return {
       connection: {
-          hosts: {
-              domain: 'meet.jit.si',
-              muc: 'conference.meet.jit.si' // FIXME: use XEP-0030
-          },
-          bosh: '//meet.jit.si/http-bind', // FIXME: use xep-0156 for that      
-          clientNode: 'http://jitsi.org/jitsimeet', // The name of client node advertised in XEP-0115 'c' stanza
+        hosts: {
+          domain: 'meet.jit.si',
+          muc: 'conference.meet.jit.si', 
+          focus: 'focus.meet.jit.si',
+        }, 
+        externalConnectUrl: 'https://meet.jit.si/http-pre-bind', 
+        enableP2P: true, 
+        p2p: { 
+          enabled: true, 
+          preferH264: true, 
+          disableH264: true, 
+          useStunTurn: true,
+        }, 
+        useStunTurn: true, 
+        bosh: `https://meet.jit.si/http-bind?room=liveroom`, 
+        websocket: 'wss://meet.jit.si/xmpp-websocket', 
+        clientNode: 'http://jitsi.org/jitsimeet', 
       },
       conference: {
           enableLayerSuspension: true,
