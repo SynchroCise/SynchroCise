e352f9ca91b9e270e779dd26e26d3918099e93f3
Matthew Wong <matthewwong@Matthews-MacBook-Air.local>
2021-08-06 19:19:27 -0400

    Add remoteParticipant join functionality

---
+++ src/Room/WorkoutDisplay/Participant/Participant.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from "react";
+
+const Participant = ({id, tracks}) => {
+  const videoRef = useRef();
+  const audioRef = useRef();
+
+  // Add track to room
+
+  // attach video track to DOM
+  useEffect(() => {
+    console.log(tracks)
+    tracks.forEach((track) => {
+      if (track.getType() === 'video') {
+        track.attach(videoRef.current);
+      } else if(track.getType() === 'audio') {
+        track.attach(audioRef.current);
+      }
+    });
+    return () => {
+      tracks.forEach((track) => track.detach())
+    }
+  }, [tracks]);
+
+  return (
+    <div>
+      <video ref={videoRef} autoPlay={true}/>
+      <audio ref={audioRef} autoPlay={true}/>
+    </div>
+  );
+}
+
+export default Participant;
--- src/Room/WorkoutDisplay/WorkoutDisplay.js
+++ src/Room/WorkoutDisplay/WorkoutDisplay.js
@@ -1,46 +1,63 @@
 import React, { useEffect, useRef, useState } from "react";
 import { buildOptions } from "../../utils/jitsi";
 import { useAppContext } from "../../AppContext";
+import Participant from "./Participant/Participant";
 
 const WorkoutDisplay = () => {
   const { JitsiMeetJS } = useAppContext();
-  const videoRef = useRef();
-  const audioRef = useRef();
-  const [videoTracks, setVideoTracks] = useState([]);
-  const [audioTracks, setAudioTracks] = useState([]);
-  
-  // attach video track to DOM
-  useEffect(() => {
-    const videoTrack = videoTracks[0];
-    if (videoTrack) {
-      videoTrack.attach(videoRef.current);
-      return () => {
-        videoTrack.detach();
-      };
-    }
-  }, [videoTracks]);
+  const [isJoined, setIsJoined] = useState(false);
+  const [room, setRoom] = useState(null);
+  const [localTracks, setLocalTracks] = useState([]);
+  const [participantIds, setParticipantIds] = useState(() => new Set());
+  const [remoteTracks, setRemoteTracks] = useState({});
 
-  // attach audio track to DOM
+  // once room has been initialized
   useEffect(() => {
-    const audioTrack = audioTracks[0];
-    if (audioTrack) {
-      audioTrack.attach(audioRef.current);
-      return () => {
-        audioTrack.detach();
-      };
+    const onRemoteTrack = (track) => {
+      console.log('onRemoteTrack');
+      const participantId = track.getParticipantId();
+      let prevTracks = [];
+      if (remoteTracks[participantId]) {
+        prevTracks = [...remoteTracks[participantId]];
+      }  
+      setRemoteTracks((prev) => ({...prev, [participantId]: [...prevTracks, track] }));      
     }
-  }, [audioTracks]);
+    const onConferenceJoined = () => {
+      console.log('onConferenceJoined', room.myUserId());
+      setParticipantIds((prev) => [...prev, room.myUserId()]);
+      localTracks.forEach((track) => room.addTrack(track));
+      setIsJoined(true);
+    };
+    const onUserJoined = (id) => {
+      console.log('onUserJoined', id);
+      setParticipantIds(prev => new Set(prev).add(id));
+    }
+    const onUserLeft = (id) => {
+      console.log('onUserLeft');
+      setParticipantIds(prev => {
+        const next = new Set(prev);
+        next.delete(id);
+        return next
+      });
+    }
+    if (room) {
+      room.join()
+      room.on(JitsiMeetJS.events.conference.TRACK_ADDED, (track) => !track.isLocal() && onRemoteTrack(track));
+      room.on(JitsiMeetJS.events.conference.CONFERENCE_JOINED, onConferenceJoined);
+      room.on(JitsiMeetJS.events.conference.USER_JOINED, onUserJoined);
+      room.on(JitsiMeetJS.events.conference.USER_LEFT, onUserLeft);
+      room.setReceiverVideoConstraint(720);
+    }
+  }, [room]);
 
   useEffect(() => {
     let connection;
-    let room;
     const roomName = 'hello';
     const tenant = 'world';
     const options = buildOptions(roomName, tenant)
     const onConnectionSuccess = () => {
       console.log('jitsi success');
-      room = connection.initJitsiConference(roomName, options.conference);
-      room.join();
+      setRoom(connection.initJitsiConference(roomName, options.conference));
     }
     const onConnectionFailed = () => {
       console.log('jitsi failed');
@@ -49,13 +66,11 @@ const WorkoutDisplay = () => {
       console.log('jitsi disconnect');
     }
     const onLocalTracks = (tracks) => {
-      tracks.forEach((track) => {
-        if (track.getType() === "video") {
-          setVideoTracks((videoTracks) => [...videoTracks, track]);
-        } else if (track.getType() === "audio") {
-          setAudioTracks((audioTracks) => [...audioTracks, track]);
-        }
-      });
+      console.log('setLocalTracks');
+      setLocalTracks(tracks)
+      if (isJoined) {
+        localTracks.forEach((track) => room.addTrack(track));
+      }
     }
     const startJitsi = async () => {
       await JitsiMeetJS.init()
@@ -68,7 +83,7 @@ const WorkoutDisplay = () => {
         .then(onLocalTracks)
         .catch(error => {
             throw error;
-        });
+      });
       console.log(connection)
     }
     if (JitsiMeetJS) {
@@ -78,8 +93,14 @@ const WorkoutDisplay = () => {
       
   return (
     <div>
-      <video ref={videoRef} autoPlay={true}/>
-      <audio ref={audioRef} autoPlay={true}/>
+      {[...participantIds].map((participantId) => {
+        if (participantId === room.myUserId()) {
+          return (<Participant key={participantId} id={participantId} tracks={localTracks}/>)
+        } else if (remoteTracks[participantId]) {
+          console.log('remotetracks', remoteTracks[participantId])
+          return (<Participant key={participantId} id={participantId} tracks={remoteTracks[participantId]}/>)
+        }
+      })}
     </div>
   );
 };
