ad3240d030e6ab2849807ec6dd96961af0795550
Ethan Liong <40578528+Freddington@users.noreply.github.com>
2021-08-17 16:24:50 -0400

    Fixed a bug made in #162

--- src/Home/CreateRoom/CreateRoom.js
+++ src/Home/CreateRoom/CreateRoom.js
@@ -11,7 +11,7 @@ import * as requests from "../../utils/requests"
 
 // this component renders form to be passed to VideoChat.js
 const CreateRoom = () => {
-  const { userId, connecting, username, roomName, handleSetRoomName, workout, handleSetRoom, handleUsernameChange, handleSetConnecting, handleSetWorkout, handleSetOpenAuthDialog, makeCustomRoom, createTempUser, isLoggedIn } = useAppContext()
+  const { userId, connecting, username, roomName, workout, handleSetRoom, handleUsernameChange, handleSetConnecting, handleSetOpenAuthDialog, makeCustomRoom, createTempUser, isLoggedIn } = useAppContext()
   const history = useHistory()
 
   // intialize custom room code
@@ -56,18 +56,13 @@ const CreateRoom = () => {
     }
   }
 
-  const handleLeave = () => {
-    history.push(RoutesEnum.Home);
-    handleSetRoomName("");
-  }
-
   return (
     <Box display="flex" alignItems="center" justifyContent="center" mx={12} my={6} data-test="createRoomComponent">
       <form onSubmit={handleSubmit} data-test="createRoomForm">
         <Toolbar />
         <Grid container justify="center" spacing={4} wrap="nowrap">
           <Grid item xs={1}>
-            <IconButton className={classes.blackButton} onClick={() => { handleLeave() }} data-test="backButton">
+            <IconButton className={classes.blackButton} onClick={() => { history.push(RoutesEnum.Home) }} data-test="backButton">
               <ArrowBack />
             </IconButton>
           </Grid>
